a9c71a8965ee84ef47a0fd3c91afc43759e142bc
rahul pawar <rahulrajput7747@gmail.com>
2026-05-02 13:50:14 +0530

    test: hardcoded mail for test

--- healthy/utils.py
+++ healthy/utils.py
@@ -37,7 +37,7 @@ def send_health_mail(user, subject, html_message):
             # html_message,
             "Your email client does not support HTML emails.",
             settings.EMAIL_HOST_USER,
-            ['developer.deepak25@gmail.com']  # For testing, send to a fixed email
+            [user.email]
         )
 
         # email.attach_alternative(html_content, "text/html")
